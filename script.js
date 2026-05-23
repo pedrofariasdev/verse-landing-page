fe32e90d0295b6ff5f5be96c1d0ba1c2febd532d
Pedro Farias <phl.farias27@gmail.com>
2026-05-23 19:27:00 +0100

    Resolve merge conflict

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const slides = document.querySelectorAll('.slide');
 
 let currentSlide = 0;
@@ -15,22 +14,4 @@ setInterval(() => {
 
   slides[currentSlide].classList.add('active');
 
-=======
-const slides = document.querySelectorAll('.slide');
-
-let currentSlide = 0;
-
-setInterval(() => {
-
-  slides[currentSlide].classList.remove('active');
-
-  currentSlide++;
-
-  if (currentSlide >= slides.length) {
-    currentSlide = 0;
-  }
-
-  slides[currentSlide].classList.add('active');
-
->>>>>>> 3db813c538ce31db38b64fe2f53ddcc4418d15da
 }, 4000);
