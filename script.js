e6ea046f35c24eb80176a8f13395798d3fc8775d
Pedro Farias <phl.farias27@gmail.com>
2026-05-23 19:11:53 +0100

    Primeira Versão da Verse

---
+++ script.js
@@ -0,0 +1,17 @@
+const slides = document.querySelectorAll('.slide');
+
+let currentSlide = 0;
+
+setInterval(() => {
+
+  slides[currentSlide].classList.remove('active');
+
+  currentSlide++;
+
+  if (currentSlide >= slides.length) {
+    currentSlide = 0;
+  }
+
+  slides[currentSlide].classList.add('active');
+
+}, 4000);
