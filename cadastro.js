3fb8b5a3cd68ec266eb918e56159e3d1660a0485
Pedro Farias <phl.farias27@gmail.com>
2026-06-05 19:25:24 +0100

    Atualização de Criar conta

---
+++ cadastro.js
@@ -0,0 +1,23 @@
+const cadastroForm = document.getElementById("cadastroForm");
+
+cadastroForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const fullName = document.getElementById("name").value.trim();
+
+    const nameError = document.getElementById("nameError");
+
+    nameError.textContent = "";
+
+    const partesNome = fullName
+        .split(" ")
+        .filter(parte => parte.length > 0);
+
+    if (partesNome.length < 2) {
+        nameError.textContent =
+            "Por favor, informe nome e sobrenome.";
+        return;
+    }
+
+    console.log("Nome válido:", fullName);
+});
