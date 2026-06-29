2f4911cd9aa8b4eb06a2654e954c685808c7957b
Pedro Farias <phl.farias27@gmail.com>
2026-06-29 14:35:16 +0100

    Iniciando App

---
+++ service-worker.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = "verse-cache-v1";
+
+const ASSETS_TO_CACHE = [
+  "/",
+  "/html/feed.html",
+  "/css/base/reset.css",
+  "/css/base/global.css",
+  "/css/components/navbar.css",
+  "/assets/favicon/android-chrome-192x192.png",
+  "/assets/favicon/android-chrome-512x512.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys
+          .filter((key) => key !== CACHE_NAME)
+          .map((key) => caches.delete(key))
+      )
+    )
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
