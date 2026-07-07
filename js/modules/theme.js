ca63f104d2230baccfd92839c7bbe87b4adeab00
Pedro Farias <phl.farias27@gmail.com>
2026-07-07 22:53:50 +0100

    Refinamento

---
+++ js/modules/theme.js
@@ -0,0 +1,31 @@
+function aplicarTemaSalvo() {
+  const temaSalvo = localStorage.getItem("verseTheme");
+
+  if (temaSalvo === "dark") {
+    document.body.classList.add("dark-theme");
+  } else {
+    document.body.classList.remove("dark-theme");
+  }
+}
+
+function alterarTema(tema) {
+  localStorage.setItem("verseTheme", tema);
+
+  if (tema === "dark") {
+    document.body.classList.add("dark-theme");
+  } else {
+    document.body.classList.remove("dark-theme");
+  }
+}
+
+document.addEventListener("DOMContentLoaded", aplicarTemaSalvo);
+
+const themeSelect = document.getElementById("themeSelect");
+
+if (themeSelect) {
+  themeSelect.value = localStorage.getItem("verseTheme") || "light";
+
+  themeSelect.addEventListener("change", function () {
+    alterarTema(themeSelect.value);
+  });
+}
